1a636d05a936a24a98d39d88dd8c93d368178113
Marcos Navarro <marcosnpcogollos@gmail.com>
2015-05-24 19:28:34 +0200

    Ejercicio obligatorio temario 1

---
+++ temario1_ejercicio_opcional.js
@@ -0,0 +1,16 @@
+/**
+ * Explicación de la tarea
+ * 
+ * Realizar en JavaScript para node.js un programa que saque por consola
+ * 
+ * 1) El número PI con ocho decimales
+ * 2) Dos línea en blanco
+ * 3)  La ristra de caracteres UNICODE
+ *      -> \u55e8\uff0c\u4f60\u597d\u5417
+ */
+
+var numeroPi = Math.PI;
+var string = "Número PI con ocho decimales => " + numeroPi.toFixed(8);
+string = "\n\n";
+string += "Carácteres unicode: \u55e8\uff0c\u4f60\u597d\u5417";
+console.log(string);
